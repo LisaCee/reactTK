8ae62252aa53766b4ac641104a2cf19eea3a40d6
LisaCee <lisaceedesign@gmail.com>
2019-09-20 16:19:51 -0700

    dynamic routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
 
-function Home(props) {
-  return <h1>Home Component</h1>;
-}
-function Blog(props) {
-  const {push} = props.history;
-  return <><h1>Blog Component</h1> 
-  <button onClick={() => push('/')}>back home</button></>;
-}
+const users = 
+[{id: 1, name: "Emily"},
+{id: 2, name: "Anna"},
+{id: 3, name: 'Rory'}]
 
+const fetchUser = id => Promise.resolve(
+  users.find(user => user.id == id)
+)
+
+const User = props => {
+  const {id} = props.match.params;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetchUser(id).then(user => setUser(user));
+  }, [id])
+
+  if (!user) return <div>Fetching User...</div>
+  return (<div>This is the {user.name}</div>)
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-        <Link to ='/'>Home</Link>
-        <Link to ='/blog'>Blog</Link>
-        
-        <Route exact path='/' component={Home} />
-        <Route path='/blog' children={(props) => {
-          return <h1>Blog!!!</h1>} 
-       }/>
+          <Link to='/users/1'>User 1</Link>
+          <Link to='/users/2'>User 2</Link>
+          <Link to='/users/3'>User 3</Link>
+
+          <Route path='/users/:id' component={User}/>
         </BrowserRouter>
       </header>
     </div>
